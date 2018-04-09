c9c6e5a8973ac48cbc406103c9d435934caf9089
Mossuru777 <mossuru777@gmail.com>
2018-04-09 15:09:41 +0900

    Node.js v8.9.0(LTS)以降での実行を前提とする

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
 import { Server } from "./server";
 
+if (process === undefined || require === undefined) {
+    throw Error("Node.js environment required.");
+}
+
 const server = new Server();
 server.start();
