880fb2dea532ec0874b7fe6853c9f6ec952d7c4c
Mossuru777 <mossuru777@gmail.com>
2018-04-11 00:59:28 +0900

    preinstall,installをすべてpostinstallに統合し、出力先をビルドファイルと同様dist以下になるよう変更

--- src/installer_files/postinstall.ts
+++ src/installer_files/postinstall.ts
@@ -2,6 +2,7 @@ import * as child_process from "child_process";
 import "colors";
 import * as fs from "fs";
 import * as path from "path";
+import * as semver from "semver";
 
 interface FileCopyInfo {
     readonly template_path: string;
@@ -11,6 +12,31 @@ interface FileCopyInfo {
 const CopyTemplateFiles: { [key: string]: FileCopyInfo } = {
     config: { template_path: "template/config.yml", dest_dir: "/usr/local/etc/homeautomation" }
 };
+
+// install environment check
+if (process.getuid() !== 0 || process.env["npm_config_global"] !== "true") {
+    console.error("Please re-run like `sudo npm install PACKAGE -g --production --unsafe-perm`.".bgRed);
+    process.exit(1);
+}
+
+// pm2 test flight
+try {
+    const testFlight = child_process.execSync("pm2 -v", { encoding: "utf8" });
+    console.log(testFlight);
+} catch {
+    console.error("Please install pm2 as global like `sudo npm install -g pm2@2`.".bgRed);
+    process.exit(1);
+}
+
+// pm2 version check
+const pm2Version = child_process.execSync("pm2 -v", { encoding: "utf8" }).trim();
+const pm2Expected = ">=2.4.0 <3.0.0";
+if (semver.satisfies(pm2Version, pm2Expected)) {
+    console.log("Version:", `pm2@${pm2Version}`.green, "[OK]".bgGreen);
+} else {
+    console.error("Version:", `pm2@${pm2Version}`.red, "[NG]".bgRed, "Expected:", pm2Expected);
+    process.exit(1);
+}
 
 // copy template files
 copyFileSyncIfNotExists(CopyTemplateFiles["config"]);
