e7bc5954d4b3acec107cfdfca4c44790f5089ea9
Mossuru777 <mossuru777@gmail.com>
2018-04-10 18:30:33 +0900

    pm2を使ったプロセス管理を行うよう、インストール/アンインストール時のフックスクリプトを追加

---
+++ install_files/src/preuninstall.ts
@@ -0,0 +1,22 @@
+import * as child_process from "child_process";
+import "colors";
+
+if (process.getuid() !== 0 || process.env["npm_config_global"] !== "true") {
+    console.log("Please re-run like `sudo npm uninstall PACKAGE -g`.".bgYellow);
+    process.exit(1);
+}
+
+child_process.execSync("pm2 stop pm2_process.json", {
+    stdio: [
+        null,
+        process.stdout,
+        process.stderr
+    ]
+});
+child_process.execSync("pm2 delete pm2_process.json", {
+    stdio: [
+        null,
+        process.stdout,
+        process.stderr
+    ]
+});
