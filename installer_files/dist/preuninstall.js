6d7769092306173774a42922a6ecb16586d786ef
Mossuru777 <mossuru777@gmail.com>
2018-04-11 14:26:59 +0900

    preuninstallでの環境チェックを廃止

--- installer_files/dist/preuninstall.js
+++ installer_files/dist/preuninstall.js
@@ -1,11 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const child_process = require("child_process");
-require("colors");
-if (process.getuid() !== 0 || process.env["npm_config_global"] !== "true") {
-    console.error("Please re-run like `sudo npm uninstall PACKAGE -g`.".bgRed.white);
-    process.exit(1);
-}
 child_process.execSync("pm2 stop pm2_process.json", {
     stdio: [
         null,
--- installer_files/src/preuninstall.ts
+++ installer_files/src/preuninstall.ts
@@ -1,10 +1,4 @@
 import * as child_process from "child_process";
-import "colors";
-
-if (process.getuid() !== 0 || process.env["npm_config_global"] !== "true") {
-    console.error("Please re-run like `sudo npm uninstall PACKAGE -g`.".bgRed.white);
-    process.exit(1);
-}
 
 child_process.execSync("pm2 stop pm2_process.json", {
     stdio: [
