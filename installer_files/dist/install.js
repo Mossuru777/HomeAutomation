d3b02f8be6cbe2752feca405d4ed5a1e0bff8c98
Mossuru777 <mossuru777@gmail.com>
2018-04-11 22:51:34 +0900

    install.tsのpm2チェックを一部変更

--- installer_files/dist/install.js
+++ installer_files/dist/install.js
@@ -3,8 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const child_process = require("child_process");
 const semver = require("semver");
 try {
-    const testFlight = child_process.execSync("pm2 -v", { encoding: "utf8" });
-    console.log(testFlight);
+    child_process.execSync("pm2 -v", { encoding: "utf8" });
 }
 catch (_a) {
     console.error("Please install pm2 as global like `sudo npm install -g pm2@2`.".bgRed.white);
@@ -13,10 +12,10 @@ catch (_a) {
 const pm2Version = child_process.execSync("pm2 -v", { encoding: "utf8" }).trim();
 const pm2Expected = ">=2.4.0 <3.0.0";
 if (semver.satisfies(pm2Version, pm2Expected)) {
-    console.log("Version:", `pm2@${pm2Version}`.green, "[OK]".bgGreen);
+    console.log("PM2 Version:", `${pm2Version}`.green, "[OK]".bgGreen);
 }
 else {
-    console.error("Version:", `pm2@${pm2Version}`.red, "[NG]".bgRed, "Expected:", pm2Expected);
+    console.error("PM2 Version:", `${pm2Version}`.red, "[NG]".bgRed, "Expected:", pm2Expected);
     process.exit(1);
 }
 //# sourceMappingURL=install.js.map
--- installer_files/src/install.ts
+++ installer_files/src/install.ts
@@ -3,8 +3,7 @@ import * as semver from "semver";
 
 // pm2 test flight
 try {
-    const testFlight = child_process.execSync("pm2 -v", { encoding: "utf8" });
-    console.log(testFlight);
+    child_process.execSync("pm2 -v", { encoding: "utf8" });
 } catch {
     console.error("Please install pm2 as global like `sudo npm install -g pm2@2`.".bgRed.white);
     process.exit(1);
@@ -14,8 +13,8 @@ try {
 const pm2Version = child_process.execSync("pm2 -v", { encoding: "utf8" }).trim();
 const pm2Expected = ">=2.4.0 <3.0.0";
 if (semver.satisfies(pm2Version, pm2Expected)) {
-    console.log("Version:", `pm2@${pm2Version}`.green, "[OK]".bgGreen);
+    console.log("PM2 Version:", `${pm2Version}`.green, "[OK]".bgGreen);
 } else {
-    console.error("Version:", `pm2@${pm2Version}`.red, "[NG]".bgRed, "Expected:", pm2Expected);
+    console.error("PM2 Version:", `${pm2Version}`.red, "[NG]".bgRed, "Expected:", pm2Expected);
     process.exit(1);
 }
