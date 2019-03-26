c56cea6d2ad1ad832dde2071dc85a569561252fe
Mossuru777 <mossuru777@gmail.com>
2019-03-26 15:14:15 +0900

    pm2の利用可能バージョンを更新

--- installer_files/dist/install.js
+++ installer_files/dist/install.js
@@ -1,6 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const child_process = require("child_process");
+require("colors");
 const semver = require("semver");
 try {
     child_process.execSync("pm2 -v", { encoding: "utf8" });
@@ -10,7 +11,7 @@ catch (_a) {
     process.exit(1);
 }
 const pm2Version = child_process.execSync("pm2 -v", { encoding: "utf8" }).trim();
-const pm2Expected = ">=2.4.0 <3.0.0";
+const pm2Expected = ">=2.4.0";
 if (semver.satisfies(pm2Version, pm2Expected)) {
     console.log("PM2 Version:", `${pm2Version}`.green, "[OK]".bgGreen);
 }
